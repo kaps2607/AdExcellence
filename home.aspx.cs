19f40302fb5719da6f25c1a409321e2d5aee4b80
kaps2607 <kapillund29@gmail.com>
2025-03-06 22:08:29 +0530

    Added Dockerfile for Deployment

---
+++ home.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.UI;
+
+namespace AdExcellence
+{
+    public partial class WebForm6 : Page
+    {
+        
+    }
+}
